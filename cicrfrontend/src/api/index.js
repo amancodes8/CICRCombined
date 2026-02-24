d26ef8939d4e4806a6ed0ede4f158a7a400d5324
Aman <amancodes6@gmail.com>
2026-02-24 16:53:58 +0530

    prod client done

--- cicrfrontend/src/api/index.js
+++ cicrfrontend/src/api/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
    AXIOS INSTANCE CONFIGURATION
 =============================== */
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://cicrcombined.onrender.com/api',
 
   headers: {
     'Content-Type': 'application/json',
